fb282216fb6da3d98bc7ffa4558f6ea11a314c6c
Wilton Martins <wiltonxx@gmail.com>
2017-08-08 14:01:37 -0300

    Exercise 2.19 answered.

--- Java 8 - How to Program/src/Chapter2/Exercise_02_19.java
+++ Java 8 - How to Program/src/Chapter2/Exercise_02_19.java
@@ -1,22 +1,26 @@
 // Java 8 - How to program 10th Ed.
+/*  Date: 08/08/2017 (dd-mm-yyyy)
+ *	2.19 What does the following code print?
+ *	System.out.printf("*%n**%n***%n****%n*****%n");
+ */
 
+// Declaring package
+package Chapter2;
 
+// Creating exercise 2.16 class
+public class Exercise_02_19
+{
+	// Creating main method to begin execution of the Java program
+	public static void main(String[] args)
+	{
+		System.out.printf("*%n**%n***%n****%n*****%n");
+	}
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*  2.19 What does the following code print?
-	System.out.printf("*%n**%n***%n****%n*****%n");
-*/
+/* output: 
+	* 
+	**
+	***
+	****
+	*****
+*/ 
