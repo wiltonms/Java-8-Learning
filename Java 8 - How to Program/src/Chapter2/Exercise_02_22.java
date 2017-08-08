8502fa419cd6501d7a340252cb4dd85b5f60294c
Wilton Martins <wiltonxx@gmail.com>
2017-08-08 14:05:48 -0300

    Exercise 2.22 answered.

--- Java 8 - How to Program/src/Chapter2/Exercise_02_22.java
+++ Java 8 - How to Program/src/Chapter2/Exercise_02_22.java
@@ -1,26 +1,34 @@
 // Java 8 - How to program 10th Ed.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*  2.22 What does the following code print?
-	System.out.print("*");
+/*  Date: 08/08/2017 (dd-mm-yyyy)
+  2.22 What does the following code print?
+ 	System.out.print("*");
 	System.out.println("***");
 	System.out.println("*****");
 	System.out.print("****");
 	System.out.println("**");
+*/
+
+// Declaring package
+package Chapter2;
+
+// Creating exercise 2.22 class
+public class Exercise_02_22
+{
+	// Creating main method to begin execution of the Java program
+	public static void main(String[] args)
+	{
+	 	System.out.print("*");
+		System.out.println("***");
+		System.out.println("*****");
+		System.out.print("****");
+		System.out.println("**");
+	}
+}
+
+/* output:
+
+****
+*****
+******
+
 */
