34f67d2b59a0cfab17819920816c8d5acbabfe72
Wilton Martins <wiltonxx@gmail.com>
2017-08-08 10:26:34 -0300

    Answers for Exercise 2.09

--- Java 8 - How to Program/src/Chapter2/Exercise_02_09.java
+++ Java 8 - How to Program/src/Chapter2/Exercise_02_09.java
@@ -1,29 +1,17 @@
 // Java 8 - How to program 10th Ed.
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /*  TASK 2.9 State whether each of the following is true or false. If false, explain why.
 
 a) Java operators are evaluated from left to right.
+[Ans.] All binary operators except for the assignment operators are evaluated from left to right; assignment operators are evaluated right to left.
 
 b) The following are all valid variable names: _under_bar_, m928134, t5, j7, her_sales$, his_$account_total, a, b$, c, z and z2.
+[Ans.] True, because valid variable names are a series of underscores, letters and digits that do not start with a digit, and none of these conditions were violated.
 
 c) A valid Java arithmetic expression with no parentheses is evaluated from left to right.
+[Ans.] False, it will be evaluated according to their precedence and associativity.
 
 d) The following are all invalid variable names: 3g, 87, 67h2, h22 and 2h.
+[Ans.] False, h22 is a valid variable name. All of the other variable names start with digits and are therefore invalid variable names.
+
 */
