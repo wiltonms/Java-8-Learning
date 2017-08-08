1b93cd4a3ce15e9b2a56e291a2563899f26c5719
Wilton Martins <wiltonxx@gmail.com>
2017-08-08 10:37:53 -0300

    Exercise 2.11 answered.

--- Java 8 - How to Program/src/Chapter2/Exercise_02_11.java
+++ Java 8 - How to Program/src/Chapter2/Exercise_02_11.java
@@ -1,22 +1,5 @@
 // Java 8 - How to program 10th Ed.
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /*  TASK 2.11 Which of the following Java statements contain variables whose values are modified?
 
 a) p = i + j + k + 7;
@@ -26,4 +9,6 @@ b) System.out.println("variables whose values are modified");
 c) System.out.println("a = 5");
 
 d) value = input.nextInt();
+
+[Ans.] A and D
 */
